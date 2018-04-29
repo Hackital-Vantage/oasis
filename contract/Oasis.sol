43be1d5f29a70a40a35025c5484f16b8223d460c
Kishore Bhatia <kishore.bhatia@bcgdv.com>
2018-04-29 02:41:48 -0400

    updated contract

--- contract/Oasis.sol
+++ contract/Oasis.sol
@@ -14,10 +14,10 @@ contract Oasis is Ownable {
 	}
     mapping(address => Player) playersMap;
 	
-	bytes32 question1 = "name of NYC river?";
-	bytes32 question2 = "name of DC river?";
-	bytes32 answer1 = "hudson";
-	bytes32 answer2 = "potomac";
+	uint question1 = 1;
+	uint question2 = 2;
+	uint answer1 = 11;
+	uint answer2 = 22;
 	
 	enum GameState { Running, Stopped }
 	GameState gameState;
@@ -28,14 +28,14 @@ contract Oasis is Ownable {
 		//fund_balance = msg.value;
 	}
 	
-	function gameSet (bytes32 _question1, bytes32 _question2, bytes32 _answer1, bytes32 _answer2 ) external onlyOwner {
+	function gameSet (uint _question1, uint _question2, uint _answer1, uint _answer2 ) external onlyOwner {
 	     question1 = _question1;
 	     question2 = _question2;
 	     answer1 = _answer1;
 	     answer2 = _answer2;
     }
   
-	function getQuestion(uint _question) public payable returns (bytes32 question) {
+	function getQuestion(uint _question) public payable returns (uint) {
 		require (msg.value == fee);
 		//players.push(Player(_name, msg.value, msg.sender, 0));
 		if (_question == 1) {
@@ -46,7 +46,7 @@ contract Oasis is Ownable {
 		}
 	}
 	
-	function checkAndTakeOwnership (bytes32 _answer) public returns (string) {
+	function checkAndTakeOwnership (uint _answer) public returns (string) {
 		//players.push(Player(_name, msg.value, msg.sender, 0));
 		if (_answer == answer1) {
 		   playersMap[msg.sender].correctAnswer += 1; 
@@ -58,6 +58,10 @@ contract Oasis is Ownable {
         address owner = msg.sender;
         address admin = msg.sender;
         return ("QmRFgaFQuHrENKxbgVjWY1g5oNMbQcGz9N6PvyhZePLEqG");
+	}
+	
+	function killWorld () external onlyOwner {
+        selfdestruct(owner);    
 	}
 	
 }
